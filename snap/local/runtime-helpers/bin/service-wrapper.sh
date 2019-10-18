2e8139a694a4281ad1d9f51198ed99d60fca5b57
Tony Espy <espy@canonical.com>
2019-10-18 18:31:00 -0400

    Add snap packaging
    Fixes: https://github.com/edgexfoundry/app-service-configurable/issues/1
    
    Signed-off-by: Tony Espy <espy@canonical.com>

---
+++ snap/local/runtime-helpers/bin/service-wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+PROFILE_OPT=""
+
+profile=$(snapctl get "profile")
+
+if [ ! -z "$profile" ]; then
+    if [ "$profile" != "default" ]; then
+        PROFILE_OPT="-profile $profile"
+    fi
+fi
+
+$SNAP/bin/app-service-configurable -confdir $SNAP_DATA/config/res $PROFILE_OPT --registry
+
