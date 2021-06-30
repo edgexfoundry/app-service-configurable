b6f81afb00c623cfafb3d4c5994f810f91048a0e
Tony Espy <tonyespy@users.noreply.github.com>
2021-06-30 11:21:43 -0400

    fix(snap): fixup env config overrides (#299)
    When we forward ported the env config override
    support from Hanoi, we missed the code in the
    service wrapper which sources the .env file.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- snap/local/runtime-helpers/bin/startup-env-var.sh
+++ snap/local/runtime-helpers/bin/startup-env-var.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 EDGEX_STARTUP_DURATION=$(snapctl get startup-duration)
 
@@ -10,6 +10,14 @@ EDGEX_STARTUP_INTERVAL=$(snapctl get startup-interval)
 
 if [ -n "$EDGEX_STARTUP_INTERVAL" ]; then
   export EDGEX_STARTUP_INTERVAL
+fi
+
+SERVICE_ENV="$SNAP_DATA/config/res/app-service-configurable.env"
+logger "edgex service override: : SERVICE_ENV=$SERVICE_ENV"
+
+if [ -f "$SERVICE_ENV" ]; then
+    logger "edgex service override: : sourcing $SERVICE_ENV"
+    source "$SERVICE_ENV"
 fi
 
 exec "$@"
