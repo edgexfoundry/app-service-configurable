94b17e45cf4e6fecfae86380ac179a745ee14f92
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-07-27 17:48:44 +0200

    refactor: move startup options handling outside of env file loader
    Signed-off-by: Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>

--- snap/local/bin/service-wrapper.sh
+++ snap/local/bin/service-wrapper.sh
@@ -3,12 +3,21 @@
 PROFILE_OPT=""
 
 profile=$(snapctl get "profile")
-
 if [ ! -z "$profile" ]; then
     if [ "$profile" != "default" ]; then
         PROFILE_OPT="-profile $profile"
         export SECRETSTORE_TOKENFILE="$SNAP_DATA/app-$profile/secrets-token.json"
     fi
+fi
+
+EDGEX_STARTUP_DURATION=$(snapctl get startup-duration)
+if [ -n "$EDGEX_STARTUP_DURATION" ]; then
+  export EDGEX_STARTUP_DURATION
+fi
+
+EDGEX_STARTUP_INTERVAL=$(snapctl get startup-interval)
+if [ -n "$EDGEX_STARTUP_INTERVAL" ]; then
+  export EDGEX_STARTUP_INTERVAL
 fi
 
 $SNAP/bin/app-service-configurable -confdir $SNAP_DATA/config/res $PROFILE_OPT -cp -r
--- snap/local/bin/source-env-file.sh
+++ snap/local/bin/source-env-file.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -e
 
-EDGEX_STARTUP_DURATION=$(snapctl get startup-duration)
-
-if [ -n "$EDGEX_STARTUP_DURATION" ]; then
-  export EDGEX_STARTUP_DURATION
-fi
-
-EDGEX_STARTUP_INTERVAL=$(snapctl get startup-interval)
-
-if [ -n "$EDGEX_STARTUP_INTERVAL" ]; then
-  export EDGEX_STARTUP_INTERVAL
-fi
-
-# binary name == service name/key
 SERVICE="app-service-configurable"
 ENV_FILE="$SNAP_DATA/config/res/$SERVICE.env"
 TAG="edgex-$SERVICE."$(basename "$0")
