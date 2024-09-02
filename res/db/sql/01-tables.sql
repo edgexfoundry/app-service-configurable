fae340c90b373e2a95d5531951e35646ccdb79af
Lindsey Cheng <beckysocute@gmail.com>
2024-09-02 12:47:56 +0800

    fix: Add the created column in store table
    Add the created column in store table to save the stored object created timestamp.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>

--- res/db/sql/01-tables.sql
+++ res/db/sql/01-tables.sql
@@ -6,5 +6,6 @@
 -- app_svc.store is used to save StoredObject as JSONB on failure
 CREATE TABLE IF NOT EXISTS app_svc.store (
     id UUID PRIMARY KEY,
+    created timestamp NOT NULL DEFAULT now(),
     content JSONB NOT NULL
 );
